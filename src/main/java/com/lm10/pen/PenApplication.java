19567c558fbeea4756b0d0fd9c0bcdb461231716
parth.dubey <parth.dubey@naukri.com>
2023-12-21 19:15:37 +0530

    first commit for the pen design

---
+++ src/main/java/com/lm10/pen/PenApplication.java
@@ -0,0 +1,11 @@
+package com.lm10.pen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class PenApplication {
+	public static void main(String[] args) {
+		System.out.println("LM10 is the goat");
+		SpringApplication.run(PenApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lm10/pen/PenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lm10.pen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
